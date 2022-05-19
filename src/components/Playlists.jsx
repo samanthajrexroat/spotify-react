74600ccd5dd1e5c167d443071ae761893dd7bcaa
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-05-19 15:09:20 -0600

    Working on getting playlists rendered

---
+++ src/components/Playlists.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+import { useStateProvider } from '../utils/StateProvider';
+import { reducerCases } from "../utils/Constants";
+import axios from 'axios';
+import styled from "styled-components";
+
+export default function Playlists() {
+    const [{ token, playlists }, dispatch] = useStateProvider();
+    useEffect(() => {
+        const getPlaylistData = async () => {
+            const response = await axios.get(
+                "https://api.spotify.com/v1/me/playlists", 
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type" : "appliction/json", 
+                    }
+                }
+            );
+            const {items} =response.data;
+            const playlists = items.map(({ name, id }) => {
+                return { name, id };
+            });
+            dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
+            console.log(playlists)
+        };
+        getPlaylistData();
+    }, [token, dispatch]);
+  return (<Container>
+        <ul>
+            {
+                playlists.map(({ name, id }) => {
+                    
+                    return  (
+                        <li key={id}>
+                            {name}
+                        </li>)
+                    
+                })
+            }
+        </ul>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  color: #b3b3b3;
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 55vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+    li {
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+`;
---
+++ src/utils/reducer.js
@@ -0,0 +1,25 @@
+import { reducerCases } from "./Constants";
+
+export const initialState = {
+    token: null,
+    playlists: [],
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case reducerCases.SET_TOKEN: 
+            return {
+                ...state, 
+                token: action.token
+            }
+        case reducerCases.SET_PLAYSLISTS:
+            return {
+                ...state,
+                playlists: action.playlists,
+            }
+        default: 
+            return state;
+    }
+};
+
+export default reducer;
