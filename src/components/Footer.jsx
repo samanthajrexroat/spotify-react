cb2ff0c4077feb2cb2c339c2ab1f8c077f50dd6e
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-05-23 16:41:07 -0600

    added footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,4 +9,11 @@ export default function Footer() {
 const Container = styled.div`
     background-color: #181818;
     height: 100%;
+    width: 100%;
+    border-top: 1px solid #282828;
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1rem;
 `;
